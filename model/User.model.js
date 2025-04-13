1404ea31ead7e9ac3449ac25d0eac1fe3894b97b
Kushal Vardhan <vardhank140102@gmail.com>
2025-04-13 23:00:30 +0530

    learned to make model

---
+++ model/User.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const userSchema = new Mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    },
+    isVerified:{
+        type:Boolean,
+        default:false,
+    },
+    verificationToken:{
+        type:String,
+    },
+    resetPasswordToken:{
+        type:String,
+    },
+    resetPasswordExpires:{
+        type:Date,
+    },
+    
+},{
+    timestamps:true,            
+})
+
+const User = mongoose.model("User", userSchema)
+
+export default User 
