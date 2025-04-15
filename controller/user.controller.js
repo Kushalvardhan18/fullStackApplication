0f6a9fba25360f76cc4dbf4df1d20e6ace1efd9a
Kushal Vardhan <vardhank140102@gmail.com>
2025-04-15 13:19:42 +0530

    registering user if not present and sending mail

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../model/User.model.js"
 import crypto from "crypto"
+import nodemailer from "nodemailer"
 
 const registerUser = async (req, res) => {
     // get data  
@@ -31,19 +32,49 @@ const registerUser = async (req, res) => {
             password
         })
         console.log(user);
-        
-        if(!user){
+
+        if (!user) {
             return res.status(400).json({
                 message: "User not registered"
             })
         }
-       const token =  crypto.randomBytes(32).toString("hex")
-       console.log(token);
-       user.verificationToken = token
-       await user.save()
-       
-    } catch (error) {
+        const token = crypto.randomBytes(32).toString("hex")
+        console.log(token);
+        user.verificationToken = token
 
+        await user.save()
+
+        // send email
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAILTRAP_HOST,
+            port: process.env.MAILTRAP_PORT,
+            secure: false, // true for port 465, false for other ports
+            auth: {
+                user:  process.env.MAILTRAP_USERNAME,
+                pass: process.env.MAILTRAP_PASSWORD
+            },
+        });
+
+        const mailOption = {
+            from: process.env.MAILTRAP_SENDEREMAIL, // sender address
+            to: user.email, // list of receivers
+            subject: "Verify your email ", // Subject line
+            text: `Please click on the following link 
+            ${process.env.BASE_URL}/api/v1/users/verify/${token}
+            `
+          }
+         await  transporter.sendMail(mailOption)
+         res.status(201).json({
+            message:"User registered successfully",
+            success:true
+         })
+          
+    } catch (error) {
+        res.status(400).json({
+            message:"User not registered",
+            error,
+            success:false
+         })
     }
 
 }
