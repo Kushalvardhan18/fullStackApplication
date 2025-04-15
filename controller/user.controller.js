c2cb72c17485a3484889caacbf2d801c69b8d240
Kushal Vardhan <vardhank140102@gmail.com>
2025-04-15 11:56:19 +0530

    validations and verifications

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,50 @@
+import User from "../model/User.model.js"
+import crypto from "crypto"
+
 const registerUser = async (req, res) => {
-    res.send("registered")
+    // get data  
+    // validate
+    // check if user already exists
+    // create a user in database
+    // create a verification token
+    // save token in database
+    // send token as email to user
+    // send success status to user
+
+    // res.send("hello")
+    const { name, email, password } = req.body
+    if (!name || !email || !password) {
+        return res.status(400).json({
+            message: "All fields are required"
+        })
+    }
+    try {
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            })
+        }
+        const user = await User.create({
+            name,
+            email,
+            password
+        })
+        console.log(user);
+        
+        if(!user){
+            return res.status(400).json({
+                message: "User not registered"
+            })
+        }
+       const token =  crypto.randomBytes(32).toString("hex")
+       console.log(token);
+       user.verificationToken = token
+       await user.save()
+       
+    } catch (error) {
+
+    }
+
 }
-export {registerUser}
+export { registerUser }
